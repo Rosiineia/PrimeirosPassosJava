711c7895998675d74ee45d43808d8433889eb87f
Rosineia <neyasantoss@gmail.com>
2021-11-25 14:31:50 -0300

    ProjetoJava

---
+++ PrimieiroAppJava/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Primeira Linha para o app");//comentario
+    System.out.println("Segunda  Linha para o app");
+    System.out.println("terceira Linha para o app");
+    }
+}
