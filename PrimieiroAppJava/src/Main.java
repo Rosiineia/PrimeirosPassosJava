6dce007bee41bc42075bfb773df14926886274b4
Jesus <rosineia.jesus@capgemini.com>
2021-11-25 16:31:25 -0300

    Attualização proj

--- PrimieiroAppJava/src/Main.java
+++ PrimieiroAppJava/src/Main.java
@@ -4,5 +4,29 @@ public class Main {
 	System.out.println("Primeira Linha para o app");//comentario
     System.out.println("Segunda  Linha para o app");
     System.out.println("terceira Linha para o app");
+
+    double value1 =100.0d;
+    double value2 = 50.0d;
+    double result = 0.0d;
+    char opCode = 's';
+
+    if(opCode == 'a')
+        result = value1 + value2;
+    else if( opCode == 's')
+        result = value1 - value2;
+    else if (opCode == 'm')
+        result = value1 * value2;
+    else if( opCode == 'd')
+        result = value1 / value2;
+    System.out.println(result);
+
+    
+
+
+
+
+
+
+
     }
 }
