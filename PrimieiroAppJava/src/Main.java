eb413e78af054c2118f4c2b59dde41dd2b5f0c1c
Jesus <rosineia.jesus@capgemini.com>
2021-11-25 18:43:57 -0300

    alteração main

--- PrimieiroAppJava/src/Main.java
+++ PrimieiroAppJava/src/Main.java
@@ -7,20 +7,19 @@ public class Main {
 
     double value1 =100.0d;
     double value2 = 50.0d;
-    double result = 0.0d;
+    double result = 0;
     char opCode = 's';
 
     if(opCode == 'a')
         result = value1 + value2;
-    else if( opCode == 's')
-        result = value1 - value2;
-    else if (opCode == 'm')
-        result = value1 * value2;
-    else if( opCode == 'd')
-        result = value1 / value2;
+    else result = value1 - value2;
     System.out.println(result);
 
-    
+
+
+
+
+
 
 
 
