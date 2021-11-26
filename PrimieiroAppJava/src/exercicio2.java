7f4e3dfc653492800e36ba121be264d292795a5a
Jesus <rosineia.jesus@capgemini.com>
2021-11-25 22:13:15 -0300

    Exercicios 1 e 2

---
+++ PrimieiroAppJava/src/exercicio2.java
@@ -0,0 +1,22 @@
+public class exercicio2 {
+    public static void main(String[] args) {
+        /*Crie um algoritmo que possua as variaveis salario, sexo(M OU F), Idade, estado civil, nome
+        Imprima: O Trabalhador (a) <nome> do sexo <sexo>, idade <idade> estado civil <estado civil>  e salario <salario>
+        encontra-se empregado nesse estabelecimento*/
+
+        String nome = "Marta Conceição";
+        int idade = 25;
+        String estadoCivil = "casado";
+        String  salario= "R$ 5.000";
+        String sexo = "F";
+
+        System.out.println("O Trabalhador (a) " + nome + " do sexo " + sexo + " idade " + idade + " estado civil " + estadoCivil +
+                " e salario " + salario + " encontra-se empregado nesse estabelecimento.");
+
+
+    }
+
+}
+
+
+
---
+++ PrimieiroAppJava/src/exercicios.java
@@ -0,0 +1,21 @@
+public class exercicios {
+    public static void main(String[] args) {
+        /*Crie um algoritimo que possua variaveis para salvar os seguintes dados:
+        Nome; Endereço e telefone de uma pessoa
+        imprima essas variaveis da seguinte fomra:
+
+        O <nome> domiciliado no enderço <endereço> e <tefone> não possue nenhum tipo de pendencia*/
+
+        String telefone = "(21)32815974";
+        String nome = "Angela Maria fernandes";
+        String endereco = "Rua das hortencias";
+
+
+        System.out.println("A Moradora  " + nome + " domiciliado(a) no endereço " + endereco + " e com telefone: " + telefone + " não possue nenhum tipo de pendência ");
+
+    }
+
+}
+
+
+
