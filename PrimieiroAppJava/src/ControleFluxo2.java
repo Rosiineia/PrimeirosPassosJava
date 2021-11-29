1e82f334ac2a169f5bfff08de053e968cb34f23e
Jesus <rosineia.jesus@capgemini.com>
2021-11-29 14:24:46 -0300

    Atualizando exercicios IF ELSE

--- PrimieiroAppJava/src/ControleFluxo2.java
+++ PrimieiroAppJava/src/ControleFluxo2.java
@@ -3,24 +3,49 @@ public class ControleFluxo2 {
 
         /*Saber se é maior de idade*/
 
-        /*int idade= 10;
+       /*int idade= 10;
 
         if (idade <18){
             System.out.println("Você é de menor");
         }else {
         System.out.println( " Você de maior");
         }*/
-        int idade= 17;
+        int idade= 64;
         String status;
 
         if (idade <=17){
-            status = "Você é de menor";
+            status = " Menor";
         }else if(idade >=18 && idade <=64 ) {
-            status = "Você Adulto";
+            status = "Adulto";
         }else{
             status = " Idoso";
         }
         System.out.println(status);
+
+
+        int num=99;
+        String numero;
+
+        if(num >=100 && num<=200){
+            numero =" Você digitou um numero entre 100 e 200";
+        }else {
+         numero="Seu numero não está entre 100 e 200";
+        }
+        System.out.println(numero);
+
+        int nota1 =3;
+        int nota2=3;
+        int nota3=7;
+        int media=(nota1+nota2+nota3)/3;
+
+
+        if(media >=7){
+            System.out.println( "Aluno aprovado, Parabéns!!");
+        }else if(media >=5 && media <7) {
+            System.out.println(" Aluno em exame final!!");
+        }else {
+            System.out.println("Aluno reprovado");
+        }
 
     }
 }
